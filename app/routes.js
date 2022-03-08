cef939fedd73ff601fcaa578989777da67f2aee3
sigmaben <ben.holden@sigma.se>
2022-03-08 11:04:21 +0000

    Updating check eligibility flow

--- app/routes.js
+++ app/routes.js
@@ -3,21 +3,106 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-// Run this code when a form is submitted to 'radio-answer'
-router.post('/radio-answer', function (req, res) {
+// Run this code when a form is submitted to 'degree-answer'
+router.post('/degree-answer', function (req, res) {
 
-    // Make a variable and give it the value from 'where-do-you-live'
-    var whereDoYouLive = req.session.data['where-do-you-live']
+    // Make a variable and give it the value from 'degree'
+    var haveDegree = req.session.data['Degree']
   
     // Check whether the variable matches a condition
-    if (whereDoYouLive == "England"){
+    if (haveDegree == "Yes"){
       // Send user to next page
-      res.redirect('/question-2-textarea')
+      res.redirect('/prototype-1/question-formal-training')
     } else {
       // Send user to ineligible page
-      res.redirect('/ineligible')
+      res.redirect('/prototype-1/ineligible')
     }
   
   })
+
+  // Run this code when a form is submitted to 'formal-training-answer'
+  router.post('/formal-training-answer', function (req, res) {
+  
+      // Make a variable and give it the value from 'degree'
+      var formalTraining = req.session.data['formal-training']
+    
+      // Check whether the variable matches a condition
+      if (formalTraining == "Yes"){
+        // Send user to next page
+        res.redirect('/prototype-1/question-completed-year')
+      } else {
+        // Send user to ineligible page
+        res.redirect('/prototype-1/ineligible')
+      }
+    
+    })
+
+    // Run this code when a form is submitted to 'completed-year'
+    router.post('/completed-year-answer', function (req, res) {
+    
+        // Make a variable and give it the value from 'degree'
+        var completedYear = req.session.data['completed-year']
+      
+        // Check whether the variable matches a condition
+        if (completedYear == "Yes"){
+          // Send user to next page
+          res.redirect('/prototype-1/question-recognised-teacher')
+        } else {
+          // Send user to ineligible page
+          res.redirect('/prototype-1/ineligible')
+        }
+      
+    })
+
+    // Run this code when a form is submitted to 'recognised-teacher'
+    router.post('/recognised-teacher-answer', function (req, res) {
+    
+        // Make a variable and give it the value from 'degree'
+        var recognisedTeacher = req.session.data['recognised-teacher']
+      
+        // Check whether the variable matches a condition
+        if (recognisedTeacher == "Yes"){
+          // Send user to next page
+          res.redirect('/prototype-1/question-sen-only')
+        } else {
+          // Send user to ineligible page
+          res.redirect('/prototype-1/ineligible')
+        }
+      
+    })
+
+    // Run this code when a form is submitted to 'sen-only'
+    router.post('/sen-only-answer', function (req, res) {
+    
+        // Make a variable and give it the value from 'degree'
+        var senOnly = req.session.data['sen-only']
+      
+        // Check whether the variable matches a condition
+        if (senOnly == "No"){
+          // Send user to next page
+          res.redirect('/prototype-1/question-14-16-years')
+        } else {
+          // Send user to ineligible page
+          res.redirect('/prototype-1/ineligible')
+        }
+      
+    })
+
+    // Run this code when a form is submitted to '14-16-years'
+    router.post('/14-16-years-answer', function (req, res) {
+    
+        // Make a variable and give it the value from 'degree'
+        var fourteenSixteenYears = req.session.data['14-16-years']
+      
+        // Check whether the variable matches a condition
+        if (fourteenSixteenYears == "Yes"){
+          // Send user to next page
+          res.redirect('/prototype-1/check-eligibility-answers')
+        } else {
+          // Send user to ineligible page
+          res.redirect('/prototype-1/ineligible')
+        }
+      
+    })
 
 module.exports = router
