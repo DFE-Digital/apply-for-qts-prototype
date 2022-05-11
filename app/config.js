d5a8a24fc8c54e6b697c5e89728805af49b553c0
dankmitchell <dan@danmitchell.co.uk>
2022-05-11 10:19:24 +0100

    Update service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Get qualified teacher status',
+  serviceName: 'Apply for qualified teacher status (QTS) in England',
 
   // Default port that prototype runs on
   port: '3000',
